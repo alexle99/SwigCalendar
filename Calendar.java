92a460ba4058af968e15fe42426204a5d9d6b692
Alexander Le <alexanvl@uci.edu>
2022-01-22 01:57:34 -0800

    calendar Class

--- Calendar.java
+++ Calendar.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class Calendar {
 
+    private String name;
+    private ArrayList<Event> events;
+    Boolean publicView;
+
+    public Calendar(String n) {
+        name = n;
+        events = new ArrayList<Event>();
+        publicView = true;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addEvent(Event e) {
+        events.add(e);
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void makePublic(Boolean b) {
+        publicView = b;
+    }
+
+    public Boolean getRepeatWeekly() {
+        return publicView;
+    }
 }
